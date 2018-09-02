f485d4ad326852917d2e2a3d386f1f1a49c539c4
SiXie <Si.Xie@cern.ch>
2018-09-02 06:06:29 +0200

    fix bug: committed wrong file, now fix it

--- python/makeETLConfigs.py
+++ python/makeETLConfigs.py
@@ -1,53 +1,55 @@
 import sys
 import argparse
-import ROOT as rt
 
-import macro.macro as macro
-from macro.razorAnalysis import razorBinning, razorFitFiles
-from macro.razorMacros import import2DRazorFitHistograms
 
 if __name__ == '__main__':
-    rt.gROOT.SetBatch()
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--tag", default="Razor2016_MoriondRereco",
-            help="Analysis tag, e.g. Razor2015")
-    parser.add_argument('--box', help="choose a box", required=True)
-    parser.add_argument('--force', help="override existing fit histograms",
-            action='store_true')
-    # Specifying a different input directory of fit file
-    parser.add_argument('--no-sys', help='no systematics', 
-            action='store_true', dest='noSys')
-    parser.add_argument('--in-dir', help='input directory',
-            dest='inDir')
-    parser.add_argument('--fit-file', help='file with fit toys',
-            dest='fitFile')
-    args = parser.parse_args()
     
-    c = rt.TCanvas("c","c",400,300) # dummy canvas
-    bins = razorBinning[args.box]
-    btagsMax = 3
-    if args.box in ['DiJet','LeptonJet']:
-        btagsMax = 2
+    parser = argparse.ArgumentParser()
+    #parser.add_argument("--tag", default="Razor2016_MoriondRereco",
+    #        help="Analysis tag, e.g. Razor2015")
+    #args = parser.parse_args()
+    
+    shapingTimeOptions = [0.5,1.0,1.5,2.0,2.5,3.0,4.0]
+    SNROptions = [5,10,15,20,30,40,50,100,1000]
+    LGADPulseLibraryOptions = ['',"LGADPulseLibrary_35micron_Gain15_Prerad.root","LGADPulseLibrary_55micron_Gain15_Prerad.root","LGADPulseLibrary_75micron_Gain15_Prerad.root","LGADPulseLibrary_W6_55micron_Gain16_5E14_480V.root","LGADPulseLibrary_W6_55micron_Gain10_1E15_550V.root","LGADPulseLibrary_W6_55micron_Gain4_3E15_700V.root"]
 
-    # Input directory
-    baseDir = "Plots/%s"%args.tag
-    if args.inDir is not None:
-        baseDir = args.inDir
+    shapingTimeName = {
+	0.5 : "0p5",
+        1.0 : "1p0",
+        1.5 : "1p5",
+        2.0 : "2p0",
+        2.5 : "2p5",
+        3.0 : "3p0",
+        4.0 : "4p0"
+        }  
 
-    # Fit file
-    fitFile = razorFitFiles[args.tag][args.box]
-    if args.fitFile is not None:
-        fitFile = args.fitFile
+    LGADPulseLibraryName = {
+		"" : "DefaultFixedSignal",
+		"LGADPulseLibrary_35micron_Gain15_Prerad.root" : "35MicronGain15Prerad",
+		"LGADPulseLibrary_55micron_Gain15_Prerad.root" : "55MicronGain15Prerad",
+		"LGADPulseLibrary_75micron_Gain15_Prerad.root" : "75MicronGain15Prerad",
+		"LGADPulseLibrary_W6_55micron_Gain16_5E14_480V.root" : "W6_55MicronGain16_5E14_480V",
+		"LGADPulseLibrary_W6_55micron_Gain10_1E15_550V.root" : "W6_55MicronGain10_1E15_550V",
+		"LGADPulseLibrary_W6_55micron_Gain4_3E15_700V.root" : "W6_55MicronGain4_3E15_700V",
+                }  
+    
 
-    for btags in range(btagsMax+1):
-        inDir = "%s/%s%dB"%(baseDir, args.box, btags)
-        if args.noSys:
-            inDir += 'NoSys'
-        inFile = "razorHistograms%s%dB.root"%(args.box, btags)
-        if args.noSys:
-            inFile = inFile.replace(".root","NoSys.root")
-        hists = macro.importHists(inDir+'/'+inFile)
+    for shapingTime in shapingTimeOptions:
+        for SNR in SNROptions:
+            for LGADPulseFile in LGADPulseLibraryOptions:
+                filename = "config/config_ShapingTime"+shapingTimeName[shapingTime]+"_SNR"+str(SNR)+"_"+LGADPulseLibraryName[LGADPulseFile]+".cfg"
+                print filename
+                f = open(filename, 'w')
+                f.write("NFilter 2 \n")
+                f.write("ShapingTime "+str(shapingTime)+"\n")
+                f.write("SNR "+str(SNR)+"\n")
+                f.write("randomSeed 1\n")
+                if (LGADPulseFile != ""):
+                    f.write("LGADSignalFilename /afs/cern.ch/work/s/sixie/public/releases/run2/Timing/CMSSW_9_0_2/src/BTL_PulseShapeSimulation/LGADPulseLibrary/" + LGADPulseFile + "\n")
+                f.close()
 
-        import2DRazorFitHistograms(hists, bins, fitFile, c, 
-                "Data", btags, btagsMax, noStat=True, force=args.force)
-        macro.exportHists(hists, outFileName=inFile, outDir=inDir)
+
+
+
+
+  
